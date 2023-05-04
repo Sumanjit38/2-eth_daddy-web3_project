906c2a1a7e909a7bda3fa85f46460a908ee66346
Sumanjit38 <sumanjit38@gmail.com>
2023-05-05 00:06:10 +0530

    first commit

---
+++ contracts/ETHDaddy.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ETHDaddy is ERC721 {
+
+    address public owner;
+    //string public name = "ETH Daddy";
+    //string public symbol = "ETHD";
+
+    //string public name = "ETH Daddy";
+    //string public symbol = "ETHD";
+
+    //TO DO
+    /// 1. List domains
+    /// 2. Buy domains
+    /// 3. Get paid
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+        //name = "ETH Daddy";
+        //name = _name;
+        //symbol = _symbol;
+
+        // add code here ....
+
+        owner = msg.sender;
+    }
+}
---
+++ test/ETHDaddy.js
@@ -0,0 +1,57 @@
+const { expect } = require("chai")
+const { ethers } = require("hardhat")
+const { string } = require("hardhat/internal/core/params/argumentTypes")
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe("ETHDaddy", () => {
+
+  let ethDaddy
+  let deployer, owner1
+  //let result
+  const NAME = "ETH Daddy"
+  const SYMBOL = "ETHD"
+
+  beforeEach(async () => {
+    //setup accounts
+    [deployer, owner1] = await ethers.getSigners();
+
+    //console.log(signers.length);
+    //console.log(signers[0].address);
+    //await ethers.getSigner();
+    
+    // deploy contract
+    const ETHDaddy = await ethers.getContractFactory('ETHDaddy')
+    ethDaddy = await ETHDaddy.deploy("ETH Daddy", "ETHD")
+
+  })
+
+  describe("Deployment", () => {
+    
+    it('has a name', async () => {
+    
+      const result = await ethDaddy.name()
+      expect(result).to.equal(NAME)
+  
+    })
+  
+    it('has a symbol', async () => {
+      
+      const result = await ethDaddy.symbol()
+      expect(result).to.equal(SYMBOL)
+  
+    })
+
+    it('has a owner', async () => {
+      
+      const result = await ethDaddy.owner()
+      expect(result).to.equal(deployer.address)
+  
+    })
+  
+  })
+
+  
+})
